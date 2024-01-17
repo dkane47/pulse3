3e8482736b80a223d22689e632be6292f65fbb7f
dkane47 <dkane47@gmail.com>
2024-01-16 20:07:02 -0700

    Style, landing page, FAQ changes

--- src/About.js
+++ src/About.js
@@ -3,13 +3,32 @@ import React from 'react';
 function About() {
     return (
       <div className="about">
+        <h1>What is Pulse?</h1>
+        <p>Pulse is a website for students to practice basic math facts.</p>
+        <h1>Why is learning math facts important?</h1>
+        <p>Answer</p>
         <h1>What makes Pulse different from other math fact practice websites?</h1>
-        <p1>Answer</p1>
+        <p1>Pulse is simple and gives students a short chunk of practice each round. 
+          It tries to figure out what fact families students could use help with, 
+          either because they got a question wrong or answered slowly, 
+          then gives them focused practice on that fact family.
+        </p1>
         <h1>Walk me through what using Pulse is like.</h1>
-        <p>I assign students to either Pulse Addition or Pulse Multiplication by linking directly</p>
+        <p>Students will click "Ready?" when they are ready to go. 
+          They will see a series of problems that progress in difficulty. 
+          In Pulse Multiplication they will see a x2 fact, then x10, x5, x1, x0, x11, x3, x4, x6, x9, x8, and x7.
+          In Pulse multiplication they will see a +1, +2, double (i.e. 6 + 6), near double (i.e. 7+8), a +3 or +4, and then a +5 through +8. 
+          If they get every question right they will progress to the next operation - 
+          for instance multiplication, fill-in-the-blank multiplication (i.e. 4 x ___ = 20), division, and fill-in-the-blank division. 
+          When the student either gets a question wrong or takes a long time to answer, they will get 30 questions of focused practice on that fact family.
+          Questions will alternate between the target fact family and previous fact families, and problems they struggle with will repeat.
+        </p>
         <h1>Do you guarantee that every student will master their math facts with Pulse?</h1>
         <p>Nope. There are lots of math fact practice tools on the internet.
-          I think Pulse is a useful tool but there is no shortcut
+          I think Pulse is a useful tool but there is no shortcut to fact fluency. 
+          Pulse works best if there is a teacher coaching students on how to use it, 
+          giving extra support to students who aren't making progress, and 
+          noticing areas where the whole class could use support and teaching those topics.
         </p>
         <h1>Sometimes students end up working on facts they already know, what's up with that?</h1>
         <p>That will happen. Pulse is fundamentally probabibilistic. Students will 
@@ -26,13 +45,13 @@ function About() {
           to assign the right link to students. I do a fair amount of coaching, both when we first start Pulse and as I 
           observe students working, to help them understand how to use it to improve their memory of math facts. 
         </p>
-        <h1>Question</h1>
+        <h1>What should I do to help my students use Pulse effectively?</h1>
         <p>Answer</p>
-        <h1>Question</h1>
+        <h1>What should I do to help students who don't make progress?</h1>
         <p>Answer</p>
-        <h1>Question</h1>
+        <h1>Why not have students sign in and track their progress?</h1>
         <p>Answer</p>
-        <h1>Question</h1>
+        <h1>Why not make it a game?</h1>
         <p>Answer</p>
         <h1>Question</h1>
         <p>Answer</p>
--- src/Home.js
+++ src/Home.js
@@ -5,16 +5,17 @@ function Home() {
     const navigate = useNavigate();
     return (
       <div className="container">
-        <h1>Ready to practice?</h1>
-        <div>
-        <button className="launch-btn" onClick={() => navigate('/add')}>
-            Addition
-        </button>
-        <button className="launch-btn" onClick={() => navigate('/multiply')}>
-            Multiplication
-        </button>
+        <h1 className="title">Pulse</h1>
+        <h2>Ready to practice?</h2>
+        <div className="button-container">
+          <button className="launch-btn" onClick={() => navigate('/add')}>
+              Addition
+          </button>
+          <button className="launch-btn" onClick={() => navigate('/multiply')}>
+              Multiplication
+          </button>
         </div>
-        <h1>Want to learn more about Pulse?</h1>
+        <h2>Want to learn more about Pulse?</h2>
         <button className="launch-btn" onClick={() => navigate('/about')}>
             About
         </button>
