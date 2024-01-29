740b54ddcab09524d12b15cec170fadc8279b6db
dkane47 <dkane47@gmail.com>
2024-01-29 06:54:00 -0700

    About proofread edits

--- src/About.js
+++ src/About.js
@@ -7,9 +7,9 @@ function About() {
         <p>How do you respond when a student doesn't know a math fact? 
           Cognitive science tells us that the best way to secure something in long-term memory is <b>retrieval practice</b>. 
           Retrieval practice means retrieving the thing from memory, not just deriving it or repeating it to ourselves. 
-          Pulse is designed to help students retrieve facts from memory. First, it figures out what fact family students are having a hard time with. 
+          Pulse is designed to help students retrieve facts from memory. First, it figures out a fact family students are having a hard time with. 
           Then, students do some practice 
-          focused on that fact family. For any fact they don't know, either because they get it wrong or take time to figure it out, 
+          focused on that fact family. For any fact they don't know, either because they get it wrong or take too long to figure it out, 
           they practice that fact again a few questions later. 
           Over time, retrieval practice will help students remember more and more of their math facts.
         </p>
@@ -19,9 +19,9 @@ function About() {
         </p>
         <h2>Shouldn't students learn math facts with understanding? This seems like rote memorization.</h2>
         <p>Students definitely need to understand how the operations work. Pulse isn't designed to <i>teach</i> facts, it's designed to <i>practice</i> facts. 
-        Once students undertand how the different operations work, they still have a few hundred specifics facts to remember. 
+        Once students undertand how the different operations work they still have a few hundred specifics facts to remember. 
         That's a lot! You should absolutely teach your students to understand the operations. 
-        If you experience is like mine, even after students understand the operations they need more practice to commit them to long-term memory. That's what Pulse is designed for.
+        If your experience is like mine, even after students understand the operations they need more practice to commit them to long-term memory. That's what Pulse is designed for.
         </p>
         <h2>So I can put my students on the app and they'll learn their math facts?</h2>
         <p>I recommend coaching students on how to use the app. They should understand the goal is to remember 
@@ -36,7 +36,7 @@ function About() {
         <h2>Sometimes students end up working on facts they already know, what's up with that?</h2>
         <p>That will happen. Pulse is probabibilistic. Students will 
           often spend their time on a fact family they aren't as good at, but they might just have
-          been chatting with a friend as they got started and Pulse thought they didn't know a fact. That's fine. 
+          been chatting with a friend as they got started or made a silly mistake and Pulse thought they didn't know a fact. That's fine. 
           They will practice that fact family mixed with a few others, and it's still good practice even if it's not perfectly targeted that day.
         </p>
         <h2>Who are you?</h2>
@@ -49,12 +49,12 @@ function About() {
         </p>
         <h2>What should I do to help my students use Pulse effectively?</h2>
         <p>Help them understand that they should try to answer as fast as they can, but if they can't remember a fact that's fine. 
-          They'll get some focused practice on that fact. Also, help students understand that if they get facts in their 
+          They'll get some focused practice on that fact. Also, help students understand that, if they get facts in their 
           target fact family wrong or answer slowly, that fact will repeat two problems later. That's a great chance to practice remembering it!
         </p>
         <h2>What should I do to help students who don't make progress?</h2>
         <p>Teach them! Pulse is a good tool for practice once students understand how the operations work. 
-          Pulse is not a substitute for learning about how the basic operations work. 
+          Pulse is not a substitute for teaching about how the basic operations work. 
           If students aren't making progress, help them see patterns in the fact families they are most often working on. 
         </p>
         <h2>Why not have students sign in and track their progress?</h2>
@@ -64,34 +64,34 @@ function About() {
         </p>
         <h2>Why not make it a game?</h2>
         <p>Practice is important. I don't think it's helpful to pretend that practice is a game. 
-          Practice is worth doing, and we should be honest with students about that.
+          Practice is necessary for learning, and we should be honest with students about that.
         </p>
         <h2>Is there research that supports Pulse?</h2>
         <p>Pulse is informed by lots of research on retrieval practice. 
-          Research can't tell us the best way to teach multiplication facts and often leaves teachers with more questions than answers. 
+          Research hasn't found the perfect way to teach multiplication facts, so reasonable people might disagree about the best way to structure fact practice based on research. 
           Pulse is designed using a mix of research and classroom experience. 
         </p>
         <h2>Why not use visuals to show how multiplication works?</h2>
-        <p>Visuals are a great tool to help students understand how operations work when they're initiall introduced. 
-          That's not what Pulse is designed for. Pulse is designed for students who have been introduced to arithemtic operations, 
-          but need more practice to commit those facts to memory. For students in that situation, visuals can be an obstacle 
-          because they let the student solve by thinking about the visual, rather than retrieving the fact from memory.
+        <p>Visuals are a great tool to help students understand how operations work when they're initially introduced. 
+          That's not what Pulse is designed for. Pulse is designed for students who have been introduced to arithemtic operations 
+          and need more practice to commit those facts to memory. For students in that situation, visuals can be an obstacle 
+          because they encourage students to solve by thinking about the visual rather than retrieving the fact from memory.
         </p>
         <h2>Can you give a more detailed description of how Pulse works?</h2>
         <p>Sure. Here's the basic sequence.</p>
         <ul>
-          <li>Click on the link to addition or multiplication practice, or give students a direct link to the right practice page.</li>
+          <li>Click on the link to addition or multiplication practice, or give students a direct link to the practice page.</li>
           <li>Click "Ready?" when you're ready to go. The goal is to solve the problems quickly, so make sure students are ready!</li>
-          <li>Students will get a sequence of 45 problems.</li>
+          <li>Students will see a sequence of 45 problems.</li>
           <li>They will start in analyzing mode. Analyzing mode gives students one question each from a series of fact families. 
-            Families are 2s, 10s, 5s, 1s, 0s, 11s, 4s, 3s, 6s, 9s, 8s, and 7s for multiplication. 
-            Families are +1s, +2s, doubles, near doubles &lt;i.e. 5+6 or 7+8&gt; +0s, 3s and 4s, and 5s through 8s.</li>
+            Families for multiplication are 2s, 10s, 5s, 1s, 0s, 11s, 4s, 3s, 6s, 9s, 8s, and 7s. 
+            Families for addition are +1s, +2s, doubles, near doubles (i.e. 5+6 or 7+8) +0s, 3s and 4s, and 5s through 8s.</li>
           <li>If students finish all the fact families they "level up," for instance from multiplication to division, then "fill-in-the-blank" multiplication, then "fill-in-the-blank" division.</li>
           <li>Students have 3.5 seconds to answer each question for the first operation, though there's no visible timer on the screen. This time decreases slowly as they move through more operations</li>
-          <li>If students get a question wrong or answer slowly, they switch to practice mode for that problem.</li>
+          <li>If students get a question wrong or answer slowly they switch to practice mode for that problem.</li>
           <li>In practice mode, half the questions are on that fact family, and the other half are a mix of questions from fact families they've seen.</li>
           <li>If students make a mistake in the first fact family they will see practice from a mix of the first few fact families.</li>
-          <li>In practice mode, if students make a mistake or answer slowly with the fact family they are working on they will see that problem again two questions later.</li>
+          <li>In practice mode, if students make a mistake or answer slowly on the fact family they are working on they will see that problem again two questions later.</li>
           <li>Problems are random, but students will typically see problems from that fact family multiple times each in a pracice session to encourage retrieval practice.</li>
           <li>After 45 problems, students will see a message saying "Nice, you're done for today." That's it!</li>
         </ul>
